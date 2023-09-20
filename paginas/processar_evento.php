93d5e1458ea257f69b792ccc21dae1a10caebaac
Maria Júlia <105013681+majudlorenzoni@users.noreply.github.com>
2023-09-20 17:55:45 -0300

    Update processar_evento.php

--- paginas/processar_evento.php
+++ paginas/processar_evento.php
@@ -8,6 +8,7 @@
         $duracao_evento = $_POST["duracao"];
         $classificacao_evento = $_POST["classificacao"];
         $webSite_evento = $_POST["website"];
+        $precobase_evento = $_POST['precobase'];
         $cnpj_organizador_evento = $_POST["cnpj"];
 
         $categoria_evento = $_POST["nomecategoriaevento"];
@@ -25,43 +26,50 @@
         include("conexao.php");
         $conexao = conectarAoBanco();
 
+       // Inserir o endereço
         $sql = "INSERT INTO plataformaCompraOnlineIngressos.endereco(rua, numero, cidade, CEP) 
         VALUES ('$rua_endereco', $numero_endereco, ' $cidade_endereco', '$cep_endereco') RETURNING idendereco;";
-        $retorno = $conexao->query($sql);  // salva id do endereço
+        $retorno = $conexao->query($sql);
 
         // Verifica se a inserção foi bem-sucedida
         if ($retorno) {
-            $row = $retorno->fetch(PDO::FETCH_ASSOC);
-            $id_endereco = $row['idendereco'];
+        $row = $retorno->fetch(PDO::FETCH_ASSOC);
+        $id_endereco = $row['idendereco'];
 
-            $sql = "INSERT INTO plataformaCompraOnlineIngressos.categoriaevento(nomecategoriaevento, descricao)
+        // Inserir a categoria do evento
+        $sql = "INSERT INTO plataformaCompraOnlineIngressos.categoriaevento(nomecategoriaevento, descricaocategoria)
             VALUES ('$categoria_evento', '$descricao_categoria_evento');";
-            $retorno_categoria = $conexao->query($sql);
+        $retorno_categoria = $conexao->query($sql);
 
-            if ($retorno_categoria) {
-                $sql = "INSERT INTO plataformaCompraOnlineIngressos.evento
-                (titulo, datahoraevento, descricao, duracao, classificacao, website, cnpj, nomecategoriaevento, nomelocal, IDendereco)
-                 VALUES ('$titulo_evento', '$dataHora_evento', 
-                 '$descricao_evento',  '$duracao_evento', '$classificacao_evento',
-                 '$webSite_evento', ' $cnpj_organizador_evento', '$categoria_evento', '$nomelocal_evento', $id_endereco);";
-                $retorno_evento = $conexao->query($sql);
+        if ($retorno_categoria) {
+        // Inserir o local do evento
+        $sql = "INSERT INTO plataformaCompraOnlineIngressos.localevento(nomelocal, idendereco, detalhesdeacesso, capacidade)
+                VALUES ('$nomelocal_evento', $id_endereco, '$detalhesAcesso', $capacidadeLocal);";
+        $retorno_local = $conexao->query($sql);
 
-                if ($retorno_evento) {
-                    header("Location: consultar_eventos.php");   // direciona para página de login
-                    exit();
-                } else {
-                    echo "Erro ao inserir evento. ";
-                }
+        if ($retorno_local) {
+            // Inserir os dados do evento
+            $sql = "INSERT INTO plataformaCompraOnlineIngressos.evento
+                    (titulo, datahoraevento, descricao, duracao, classificacao, website, precobase, cnpj, nomecategoriaevento, nomelocal, IDendereco)
+                    VALUES ('$titulo_evento', '$dataHora_evento', 
+                    '$descricao_evento',  '$duracao_evento', '$classificacao_evento',
+                    '$webSite_evento', $precobase_evento, '$cnpj_organizador_evento', '$categoria_evento', '$nomelocal_evento', $id_endereco);";
+            $retorno_evento = $conexao->query($sql);
+
+            if ($retorno_evento) {
+                header("Location: consultar_eventos.php");
             } else {
-                echo "Erro ao inserir categoria de evento. ";
+                echo "Erro ao inserir dados do evento: " . $conexao->errorInfo()[2];
             }
         } else {
-            echo "Erro ao inserir endereço do evento. ";
+            echo "Erro ao inserir dados do local: " . $conexao->errorInfo()[2];
         }
-    } else {
-        // Se o formulário não foi submetido
-        echo "O formulário não foi submetido.";
-    }   
-
+        } else {
+        echo "Erro ao inserir categoria do evento: " . $conexao->errorInfo()[2];
+        }
+        } else {
+        echo "Erro ao inserir endereço: " . $conexao->errorInfo()[2];
+        }
+    }
     $conexao = null;
 ?>
